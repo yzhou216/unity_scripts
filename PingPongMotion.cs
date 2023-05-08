80c54188bd1d40a76b3eb22e98965b301c3d06f8
Yiyu Zhou <yzhou155@md-its-210231.ds.usfca.edu>
2023-05-08 16:41:55 -0700

    Clean up

--- PingPongMotion.cs
+++ PingPongMotion.cs
@@ -6,29 +6,28 @@ public class PingPongMotion : MonoBehaviour
 {
 	private Transform ThisTransform = null;
 
-    //Original position
-    private Vector3 OrigPos = Vector3.zero;
+	/* Original position */
+	private Vector3 OrigPos = Vector3.zero;
 
-    //Axes to move on
-    public Vector3 MoveAxes = Vector2.zero;
+	/* Axes to move on */
+	public Vector3 MoveAxes = Vector2.zero;
 
-    //Speed
-    public float Distance = 3f;
-    //--------------------------------
-    // Use this for initialization
-    void Awake()
-    {
-        //Get transform component
-        ThisTransform = GetComponent<Transform>();
+	/* Speed */
+	public float Distance = 3f;
 
-        //Copy original position
-        OrigPos = ThisTransform.position;
-    }
-    //--------------------------------
-    // Update is called once per frame
-    void Update()
-    {
-        //Update platform position with ping pong
-        ThisTransform.position = OrigPos + MoveAxes * Mathf.PingPong(Time.time, Distance);
-    }
+	void Awake()
+	{
+		//Get transform component
+		ThisTransform = GetComponent<Transform>();
+
+		//Copy original position
+		OrigPos = ThisTransform.position;
+	}
+
+	/* Update is called once per frame */
+	void Update()
+	{
+		/* Update platform position with ping pong */
+		ThisTransform.position = OrigPos + MoveAxes * Mathf.PingPong(Time.time, Distance);
+	}
 }
